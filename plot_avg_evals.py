a89f3a255dc4c229a868a89f1e1ac7cd08c8de78
Preston <quachpreston@gmail.com>
2024-12-06 02:22:27 -0800

    need to pull

--- plot_avg_evals.py
+++ plot_avg_evals.py
@@ -5,6 +5,7 @@ import re
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.stats import linregress
+import statsmodels.api as sm
 
 
 
@@ -36,42 +37,42 @@ def main():
     for i, csv_file in enumerate(chess_data_csv_list):
         file_path = os.path.join(input_directory, csv_file)
         df = pd.read_csv(file_path)
-                
+        df['Calc_Eval'] = df.apply(lambda x: x['Calc_Eval'].astype(float) * -1.0 if x['Player'] == 0.0 else x['Calc_Eval'].astype(float), axis = 1)
+        
         # Plot scatter plot for White players
         plt.figure(figsize=(10, 5))
-        white_data = df[df['Player'] == 1]
-        plt.scatter(white_data['calc_time'], white_data['Calc_Eval'], c=colors[i], edgecolor='black', marker='o', label=f'White (o) {csv_file}')
+        great_data = df[(df['Calc_Eval'] >= 0) & (df['calc_time'] <= 60)]
+        plt.scatter(great_data['calc_time'], great_data['Calc_Eval'], c=colors[i], edgecolor='black', marker='o', label=f'White (o) {csv_file}')
 
-        # Perform linear regression for White players
-        fit = linregress(white_data['calc_time'], white_data['Calc_Eval'])
-        regression_line = fit.slope * white_data['calc_time'] + fit.intercept
-        plt.plot(white_data['calc_time'], regression_line, color='red', linestyle='--', label=f'Regression Line (White)')
-                
+        lowess = sm.nonparametric.lowess
+        relative_great_data = great_data.groupby('calc_time').agg({'Calc_Eval': 'mean'}).reset_index()
+        loess_smoothed_great = lowess(relative_great_data['calc_time'], relative_great_data['Calc_Eval'], frac = 0.8, it= 3)
+        print(loess_smoothed_great[:,1])
+        plt.plot(relative_great_data['calc_time'], loess_smoothed_great[:,1], '-r') 
+            
+            
         plt.xlabel('Calculation Time')
         plt.ylabel('Calculation Evaluation')
-        plt.title(f'Calculation Evaluation vs. Calculation Time for White Players ({csv_file})')
+        plt.title(f'Calculation Evaluation vs. Calculation Time for Players ({csv_file})')
         plt.legend()
         #plt.show()
-        plt.savefig(f'white_players_{csv_file.replace(".csv", "")}.png')
+        plt.savefig(f'chess_data_img/improvement_players_{csv_file.replace(".csv", "")}.png')
         plt.close()
         
         # Plot scatter plot for Black players
         plt.figure(figsize=(10, 5))
-        black_data = df[df['Player'] == 0]
-        plt.scatter(black_data['calc_time'], -black_data['Calc_Eval'], c=colors[i], edgecolor='black', marker='^', label=f'Black (x) {csv_file}')
-        
-        # Perform linear regression for Black players
-        fit = linregress(black_data['calc_time'], -black_data['Calc_Eval'])
-        regression_line = fit.slope * black_data['calc_time'] + fit.intercept
-        plt.plot(black_data['calc_time'], regression_line, color='red', linestyle='--', label=f'Regression Line (Black)')
+        mistake_data = df[df['Calc_Eval'] < 0]
+        plt.scatter(mistake_data['calc_time'], mistake_data['Calc_Eval'], c=colors[i], edgecolor='black', marker='^', label=f'Black (x) {csv_file}')
+
 
         plt.xlabel('Calculation Time')
         plt.ylabel('Calculation Evaluation')
         plt.title(f'Calculation Evaluation vs. Calculation Time for Black Players ({csv_file})')
         plt.legend()
         #plt.show()
-        plt.savefig(f'black_players_{csv_file.replace(".csv", "")}.png')
+        plt.savefig(f'chess_data_img/mistake_players_{csv_file.replace(".csv", "")}.png')
         plt.close()
+        
 
 
 
