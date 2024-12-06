bb74786dfaa60e8f998b2682e20cf950fe1faa34
Preston <quachpreston@gmail.com>
2024-12-05 18:31:14 -0800

    changed logic in  evaluation graphing

--- chess_data_getperformance.py
+++ chess_data_getperformance.py
@@ -1,4 +1,5 @@
 import pandas as pd 
+import numpy as np
 import sys 
 import matplotlib.pyplot as plt
 
@@ -30,19 +31,25 @@ def caluclate_time(start_time, end_time):
         return 0
     return (time_to_seconds(end_time) - time_to_seconds(start_time)) *-1
 
-def calculate_eval(current_eval, previous_eval, turn):
+def determine_improvement(calc_eval, turn):
+    if(calc_eval == None):
+        return None 
+    if(turn%2 == 0):
+        if(float(calc_eval) * -1 > 0): return 1.0 
+        return 0.0
+    
+    if(float(calc_eval) > 0): return 1.0 
+    return 0.0
+
+def calculate_evaluation(current_eval, previous_eval, turn):
     if('#' in str(current_eval) or '#' in str(previous_eval)):
         return None, turn%2
     elif(previous_eval is None and current_eval is None):
         return None, turn%2
     elif(previous_eval is None):
-        if(float(current_eval) > 0.0):
-            return 1, turn%2
-        return 0, turn%2
+        return float(current_eval), turn%2
 
-    if(float(current_eval)  - float(previous_eval) > 0.0):
-        return 1, turn%2
-    return 0, turn%2
+    return float(current_eval) - float(previous_eval), turn%2
 
 def get_game_performance(game):
     eval_score = game['Evaluations'].split(' ')
@@ -56,8 +63,10 @@ def get_game_performance(game):
     dataframe['Previous_Time'] = dataframe['Time'].shift(2)
     dataframe['calc_time'] = dataframe.apply(lambda x: caluclate_time(x['Previous_Time'], x['Time']), axis = 1)
 
-    dataframe['Improvement'] = dataframe.apply(lambda x: calculate_eval(x['Eval'],x['Previous_Eval'], x['Turn'])[0], axis = 1)
-    dataframe['Player'] = dataframe.apply(lambda x: calculate_eval(x['Eval'], x['Previous_Eval'], x['Turn'])[1], axis = 1)
+    dataframe['Player'] = dataframe.apply(lambda x: calculate_evaluation(x['Eval'], x['Previous_Eval'], x['Turn'])[1], axis = 1)
+    dataframe['Calc_Eval'] = dataframe.apply(lambda x: calculate_evaluation(x['Eval'], x['Previous_Eval'], x['Turn'])[0], axis = 1)
+    dataframe['Improvement'] = dataframe.apply(lambda x: determine_improvement(x['Calc_Eval'], x['Turn']), axis = 1)
+    
     dataframe.dropna(inplace=True)
     return dataframe.drop(columns=['Previous_Eval', 'Previous_Time', 'Eval', 'Time'],axis = 1)
   
@@ -78,14 +87,24 @@ def main():
 
     game_performance_white = game_performance_white.groupby('calc_time').agg({'Improvement': 'mean'}).reset_index()
     
+    #Setting Bins and Labels. Mess with it
+    bins = [0,5,10,15,20,25,30,35,40,45,50,55,60,
+            90,120,150,180,210,240,270,300]
+    
+    labels = ['0-5','5-10','10-15','15-20','20-25','25-30','30-35','35-40','40-45','45-50','50-55','55-60',
+              '60-90','90-120', '120-150','150-180','180-210','210-240','240-270','270-300']
+    
+    
+    game_performance_white['time_group'] = pd.cut(game_performance_white['calc_time'], bins = bins, labels = labels, right = False)
+    game_performance_white = game_performance_white.groupby('time_group').agg({'Improvement' : 'mean'}).reset_index()
+    
     
     plt.figure(figsize=(14, 10))
     plt.xlabel('Clock Time (seconds)')
-    plt.ylabel('Average Evaluation Score')
-    plt.title('Average Evaluation Score vs. Clock Time for Different Elo Ranges (White and Black Moves)')
-    plt.legend(title="Elo Range and Player")
+    plt.ylabel('Relative Number of Improvements')
+    plt.title('Number of Imporvements vs Clock Time')
     plt.grid(True)
-    plt.plot(game_performance_white['calc_time'], game_performance_white['Improvement'])
+    plt.plot(game_performance_white['time_group'], game_performance_white['Improvement'])
     plt.savefig('temp.png')
     
     game_performance.to_csv(output_directory + chess_data_csv_output_list[6], index = False,  encoding="utf-8")
